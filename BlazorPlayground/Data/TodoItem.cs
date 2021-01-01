2c6443f37d756695de02f43c77c8955edef8ccb9
Saw Naw <waterchan@gmail.com>
2021-01-01 17:45:31 +0000

    Add project files.

---
+++ BlazorPlayground/Data/TodoItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorPlayground.Data
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime DateAdded { get; set; }
+
+        public TodoItem(string title)
+        {
+            this.Title = title;
+            this.IsDone = false;
+            this.DateAdded = DateTime.Now;
+        }
+    }
+}
